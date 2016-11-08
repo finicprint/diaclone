18be057f22624654d01e12e8f327495e383d2862
alexcoffee <poxit@hushmail.com>
2016-11-08 15:00:55 -0800

    made DateTimeIso8601Transformer more lenient

--- src/Transformer/DateTimeIso8601Transformer.php
+++ src/Transformer/DateTimeIso8601Transformer.php
@@ -4,7 +4,10 @@ declare(strict_types = 1);
 namespace Diaclone\Transformer;
 
 use Carbon\Carbon;
+use DateTime;
+use Diaclone\Exception\TransformException;
 use Diaclone\Resource\ResourceInterface;
+use Exception;
 
 class DateTimeIso8601Transformer extends AbstractTransformer
 {
@@ -12,7 +15,32 @@ class DateTimeIso8601Transformer extends AbstractTransformer
     {
         $value = $this->getPropertyValueFromResource($resource);
 
-        return $value ? Carbon::instance($value)->toIso8601String() : '';
+        try {
+
+            if ($carbon = self::parseCarbon($value)) {
+                return $carbon->toIso8601String();
+            }
+
+            return '';
+
+        } catch (Exception $e) {
+            throw new TransformException('Failed to transform ' . $resource->getPropertyName() . ' because Carbon.');
+        }
+    }
+
+    public static function parseCarbon($value)
+    {
+        if ($value instanceof Carbon) {
+            return $value;
+        }
+
+        if ($value instanceof DateTime) {
+            return Carbon::instance($value);
+        }
+
+        if (is_string($value)) {
+            return new Carbon($value);
+        }
     }
 
     public function untransform(ResourceInterface $resource)
