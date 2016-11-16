ef1eaf1c659f5743693aaaf2970887a562f73ddf
alexcoffee <poxit@hushmail.com>
2016-11-15 21:26:07 -0800

    fixed array untransform

--- src/Transformer/ArrayTransformer.php
+++ src/Transformer/ArrayTransformer.php
@@ -14,6 +14,6 @@ class ArrayTransformer extends AbstractTransformer
 
     public function untransform(ResourceInterface $resource)
     {
-        return $resource;
+        return $this->getPropertyValueFromResource($resource);
     }
 }
