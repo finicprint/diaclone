a94edafdd4a1760929bb75ea50088ae724d6faf9
alexcoffee <poxit@hushmail.com>
2016-12-07 09:28:03 -0800

    don't add empty objects to collection

--- tests/unit/TransformNullCest.php
+++ tests/unit/TransformNullCest.php
@@ -2,6 +2,7 @@
 declare(strict_types = 1);
 
 use Diaclone\TransformService;
+use Test\Unit\Support\Entities\Person;
 use Test\Unit\Support\Transformers\PersonTransformer;
 
 class TransformNullCest
@@ -12,6 +13,104 @@ class TransformNullCest
 
         $expected = [
             'person' => [],
+        ];
+
+        $I->assertEquals($expected, $output);
+    }
+
+    public function testTransformationNullNoKey(UnitTester $I)
+    {
+        $output = (new TransformService())->transform([], new PersonTransformer());
+
+        $expected = [];
+
+        $I->assertEquals($expected, $output);
+    }
+
+    public function testTransformationNullChild(UnitTester $I)
+    {
+        $friends = [
+            new Person('Paul', 'Real estate novelist'),
+            new Person('John', 'Bartender'),
+            [],
+            null,
+        ];
+
+        $output = (new TransformService())->transform(new Person('Bill', 'Piano Man', $friends), new PersonTransformer(), 'person');
+
+        $expected = [
+            'person' => [
+                'name'       => 'My name is Bill',
+                'age'        => 42,
+                'pigLatin'   => 'Ymay amenay isyay Illbay',
+                'occupation' => [
+                    'name' => 'Piano Man',
+                ],
+                'friends'    => [
+                    [
+                        'name'       => 'My name is Paul',
+                        'age'        => 42,
+                        'pigLatin'   => 'Ymay amenay isyay Aulpay',
+                        'occupation' => [
+                            'name' => 'Real estate novelist',
+                        ],
+                        'friends'    => [],
+                    ],
+                    [
+                        'name'       => 'My name is John',
+                        'age'        => 42,
+                        'pigLatin'   => 'Ymay amenay isyay Ohnjay',
+                        'occupation' => [
+                            'name' => 'Bartender',
+                        ],
+                        'friends'    => [],
+                    ],
+                ],
+            ],
+        ];
+
+        $I->assertEquals($expected, $output);
+    }
+
+    public function testTransformationNullChildNoKey(UnitTester $I)
+    {
+        $friends = [
+            new Person('Paul', 'Real estate novelist'),
+            new Person('John', 'Bartender'),
+            [],
+            null,
+        ];
+
+        $output = (new TransformService())->transform(new Person('Bill', 'Piano Man', $friends), new PersonTransformer());
+
+        $expected = [
+            'name'       => 'My name is Bill',
+            'age'        => 42,
+            'pigLatin'   => 'Ymay amenay isyay Illbay',
+            'occupation' => [
+                'name' => 'Piano Man',
+            ],
+            'friends'    => [
+                [
+                    'name'       => 'My name is Paul',
+                    'age'        => 42,
+                    'pigLatin'   => 'Ymay amenay isyay Aulpay',
+                    'occupation' => [
+                        'name' => 'Real estate novelist',
+                    ],
+                    'friends'    => [],
+                ],
+                [
+                    'name'       => 'My name is John',
+                    'age'        => 42,
+                    'pigLatin'   => 'Ymay amenay isyay Ohnjay',
+                    'occupation' => [
+                        'name' => 'Bartender',
+                    ],
+                    'friends'    => [],
+                ],
+            ],
+
         ];
 
         $I->assertEquals($expected, $output);
