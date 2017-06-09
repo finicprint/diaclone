e3dc92347e922b5b27cec069290d062f3b66a532
Edgard <eavilesmejia@gmail.com>
2017-06-09 17:03:17 -0600

    improve ES connector

--- src/Connector/ElasticSearchConnector.php
+++ src/Connector/ElasticSearchConnector.php
@@ -11,12 +11,15 @@ class ElasticSearchConnector extends Connector
 {
     private $data;
     private $instance;
-    private $elastichsearch_params;
+    private $default_params = [
+        'index' => '',
+        'type' => ''
+    ];
 
     public function __construct($data, array $params = [], array $hosts = [])
     {
         $this->data = $data;
-        $this->elastichsearch_params = $params;
+        $this->default_params = $params;
         if (empty($this->instance)) {
             if (!empty($hosts))
                 $this->instance = ClientBuilder::create()->setHosts($hosts)->build();
@@ -52,5 +55,72 @@ class ElasticSearchConnector extends Connector
     {
         $this->data = $data;
         return $this;
+    }
+
+    public function getInstance()
+    {
+        return $this->instance;
+    }
+
+    public function createDocuement(int $id, array $data)
+    {
+        $params = $this->default_params;
+        $params['id'] = $id;
+        $params['body'] = $data;
+        //TODO: apply desearilize
+        return $this->instance->index($params);
+    }
+
+    public function getDocument(int $id)
+    {
+        $params = $this->default_params;
+        $params['id'] = $id;
+        $response = $this->instance->get($params);
+        //TODO; apply desearilize
+        return $response;
+
+    }
+
+    public function updateDocument(int $id, array $data)
+    {
+        return $this->createDocuement($id, $data);
+    }
+
+    public function searchDocument(array $query)
+    {
+        $params = $this->default_params;
+        $params['body'] = $query;
+        $response = $this->instance->search($params);
+        return $response;
+    }
+
+    public function deleteDocument(int $id)
+    {
+        $params = $this->default_params;
+        $params['id'] = $id;
+        $response = $this->instance->delete($params);
+        return $response;
+    }
+
+    public function createIndex(string $index, array $body)
+    {
+        $params = [
+            'index' => $index,
+            'body' => $body
+        ];
+
+        $response = $this->instance->indices()->create($params);
+        return $response;
+
+    }
+
+    public function deleteIndex(string $id)
+    {
+        $params = [
+            'index' => $id
+        ];
+
+        $response = $this->instance->indices()->delete($params);
+        return $response;
     }
 }
